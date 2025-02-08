9cd375548965675bcbce359ac6163baa3143725a
CK <yao.siq@northeastern.edu>
2025-02-08 00:42:15 -0500

    add: 405 when method error

--- src/routes/food.ts
+++ src/routes/food.ts
@@ -1,12 +1,16 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import { createFood, deleteFood, getAllFoods, getFoodById } from "@/controllers/food";
 import {noPayload} from "@/middleware";
 
 const router = express.Router();
+const handleMethodNotAllowed: RequestHandler = async (_, res) => {
+  res.status(405).end();
+}
 
 router.get("/", noPayload, getAllFoods);
 router.get("/:id", noPayload, getFoodById);
 router.post("/", createFood);
 router.delete("/:id", deleteFood);
+router.all("/", handleMethodNotAllowed);
 
 export default router;
