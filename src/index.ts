81acc364a397cb799f452d676569b4e59acdae9b
CK <yao.siq@northeastern.edu>
2025-03-12 19:45:27 -0400

    refactor: symbols

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
-import foodRoutes from "@/routes/plan";
+import planroutes from "@/routes/plan";
 import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api", foodRoutes);
+app.use("/api", planroutes);
 
 mongoose
-  .connect("mongodb://localhost:27017/foodLibrary")
+  .connect("mongodb://localhost:27017/plan")
   .then(() => {
     console.log("MongoDB Connected");
     app.listen(PORT, () => {
