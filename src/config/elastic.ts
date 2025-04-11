bd24a7b979fb8f8ba4dd9caf52880897122aa4bc
CK <yao.siq@northeastern.edu>
2025-04-11 13:33:40 -0400

    change: elastic config

--- src/config/elastic.ts
+++ src/config/elastic.ts
@@ -5,9 +5,6 @@ dotenv.config();
 export const elasticConfig = {
   node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200',
   auth: {
-    apiKey: {
-      id: process.env.ELASTICSEARCH_API_ID || 'foo',
-      api_key: process.env.ELASTICSEARCH_API_KEY || 'bar',
-    },
+    apiKey: process.env.ELASTICSEARCH_API_KEY,
   }
 };
